c2d30539640b285acf79f0d294658b75581a35fb
benjamindavies-png <57079052+benjamindavies-png@users.noreply.github.com>
2022-08-30 12:52:07 -0600

    Added comments
    Notes from class about project steps

--- tesla.c
+++ tesla.c
@@ -20,6 +20,14 @@ MODULE_LICENSE("GPL v2");
 asmlinkage long tesla_getdents(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count)
 {
 	//call getdents and use the information to filter out tesla files
+	
+	//step 1: total_size = orig_getdents( 3 ,dirp, ?)
+	//kmalloc(total_size, GFP_KERNEL)
+	//step 2: copy_from_user(dirp_kernel, dirp, total_size) total_size depends on whether or not tesla file occurs first, remove if first, else hide
+	//step 3: merge dirp, hide/delete tesla files
+	//step 4: copy_to_user(dirp, dirp_kernel, total_size)
+	//
+	// 
     return 0;
 }
 
